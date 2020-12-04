b68477325d26333af630b011bee86d50016c196f
Matt Daneshvar <matt@4thirteen.com.my>
2020-12-04 15:08:42 +0800

    Add a layer of styling to form elements

--- src/sailui.js
+++ src/sailui.js
@@ -2,16 +2,21 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin(function ({addComponents, addUtilities, addBase, theme, postcss}) {
   let sailTheme = require('./themes/sailTheme')({theme})
-  let formTheme = require('./themes/formTheme')({theme: sailTheme})
 
   addBase(require('./typography'))
 
-  addComponents(require('./components/btn')({theme: sailTheme}))
-  addComponents(require('./components/badge')({theme: sailTheme}))
   addComponents(require('./components/card'))
   addComponents(require('./components/alert'))
   addComponents(require('./components/container'))
+  addComponents(require('./components/btn')({theme: sailTheme}))
+  addComponents(require('./components/badge')({theme: sailTheme}))
   addComponents(require('./components/link')({theme: sailTheme}))
+
+  addComponents(require('./components/form/input')({theme: sailTheme}))
+  addComponents(require('./components/form/select')({theme: sailTheme}))
+  addComponents(require('./components/form/checkbox')({theme: sailTheme}))
+  addComponents(require('./components/form/radio')({theme: sailTheme}))
+  addComponents(require('./components/form/textarea')({theme: sailTheme}))
 
   let forms = require('@tailwindcss/forms')
   forms.handler({addBase, theme})
