265040b43316ae9c7272d045e09f65b47797f2bf
Matt Daneshvar <matt@4thirteen.com.my>
2020-12-04 15:08:42 +0800

    Add a layer of styling to form elements

---
+++ src/components/form/select.js
@@ -0,0 +1,22 @@
+const tc = require('tinycolor2')
+
+module.exports = ({theme}) => {
+  let primaryColor = theme('colors.primary')
+
+  return {
+    '.form-select': {
+      backgroundColor: '#fff',
+      borderColor: '#e2e8f0',
+      borderWidth: '1px',
+      borderRadius: '0.25rem',
+
+      '&:focus': {
+        "--tw-ring-offset-shadow": "var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color)",
+        "--tw-ring-shadow": "var(--tw-ring-inset) 0 0 0 calc(3px + var(--tw-ring-offset-width)) var(--tw-ring-color)",
+        "--tw-ring-color": tc(primaryColor).setAlpha(0.1).toString(),
+        borderColor: tc(primaryColor).setAlpha(0.6).toString(),
+        boxShadow: "var(--tw-ring-offset-shadow),var(--tw-ring-shadow),var(--tw-shadow,0 0 #0000)",
+      }
+    },
+  }
+}
